008b93a76404419a017a52014bab9aed2e90579d
lucui <1041589837@qq.com>
2018-11-23 15:58:32 +0800

    11/23

---
+++ 51FenXiang/src/views/appletDetails/index.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="main-box">
+  两张占位图
+  <img src="" alt="">
+  <img src="" alt="">
+</div>
+</template>
+
+<script>
+  // import someComponent from './someComponent'
+  export default {
+    name: "appletDetails",
+    component: {
+// someComponent
+    },
+    computed: {},
+    data() {
+      return {
+        msg: "Hello Vue.js"
+      }
+    },
+    methods: {},
+    created() {
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
