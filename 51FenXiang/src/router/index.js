e2fb156d35adcd987784bae81638916e7adfc3a7
lucui <1041589837@qq.com>
2018-11-26 09:05:15 +0800

    11/26，app详情图片预览，解决第一次进入无法滚动

--- 51FenXiang/src/router/index.js
+++ 51FenXiang/src/router/index.js
@@ -10,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'appDetails',
+      name: 'appDetail',
       component: resolve => require(['@/views/appDetails/appDetails'], resolve)
     },
     {
@@ -24,15 +24,25 @@ export default new Router({
       component: resolve => require(['@/components/common/question'], resolve)
     },
     {
-      path: '/playVideo',
-      name: 'playVideo',
-      component: resolve => require(['@/views/wx/playVideo'], resolve)
+      path: '/appletDetails',
+      name: 'appletDetails',
+      component: resolve => require(['@/views/appletDetails'], resolve)
+    },
+    {
+      path: '/newGuidance',
+      name: 'newGuidance',
+      component: resolve => require(['@/views/newGuidance'], resolve)
     },
     // {
     //   path: '/em',
     //   name: 'exa',
     //   component: resolve => require(['@/example'], resolve)
     // },
+    {
+      path: '/exe',
+      name: 'exe',
+      component: resolve => require(['@/views/exe'], resolve)
+    },
     {
       path: '/appDetails',
       name: 'appDetails',
